699d76c5e75d41e6f9a7cf767ce56a69673cbafa
pangchl <pangchunlei@yarward.com>
2026-01-05 16:03:47 +0800

    初始化

---
+++ src/main/java/com/example/geeker_admin_java/GeekerAdminJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.geeker_admin_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekerAdminJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekerAdminJavaApplication.class, args);
+	}
+
+}
